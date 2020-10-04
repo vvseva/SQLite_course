7c258db3fac5a1496a97485e5a9bec64c4374fbe
Vsevolod Suschevskiy <50490206+vvseva@users.noreply.github.com>
2020-10-04 23:48:33 +0300

    Update create1.sql

--- task1/create1.sql
+++ task1/create1.sql
@@ -1,9 +1,15 @@
+-- Write code 
+
 CREATE TABLE IF NOT EXISTS Staff(
-   full_name TEXT    PRIMARY KEY    NOT NULL,
-   bday     TEXT    NOT NULL,
-   position     TEXT    NOT NULL,
-   department     TEXT    NOT NULL,
-   supervisor  TEXT    NOT NULL
+    full_name TEXT    PRIMARY KEY    NOT NULL,
+    bday     TEXT    NOT NULL,
+    position     TEXT    NOT NULL,
+    department     TEXT    NOT NULL,
+    supervisor  TEXT    NOT NULL,
+    FOREIGN KEY (position)
+        REFERENCES Position (pos_name),
+    FOREIGN KEY (department)
+        REFERENCES Department (dep_name),
 );
 
 CREATE TABLE IF NOT EXISTS Position(
